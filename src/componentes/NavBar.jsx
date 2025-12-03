8c2778fef90fccd9e530294086ca61c32664836b
AsleepSiren4516 <bobby4096@outlook.com>
2025-12-02 19:49:12 -0600

    Hola guapote rico

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,36 +1,26 @@
 import { Link } from "react-router-dom";
+import "../css/style.css"
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
-      <Link className="navbar-brand" to="/">JAB</Link>
+    <nav className="navbar navbar-expand-lg position-absolute w-100 p-3" style={{ zIndex: 10 }}>
+      <Link className="navbar-brand text-white" to="/">JAB</Link>
 
       <button
         className="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navMenu"
-        aria-controls="navMenu"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navMenu">
         <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Inicio</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/sobre">Sobre nosotros</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/servicios">Servicios</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contacto">Contactanos</Link>
-          </li>
+          <li className="nav-item"><Link className="nav-link text-white" to="/">Inicio</Link></li>
+          <li className="nav-item"><Link className="nav-link text-white" to="/sobre">Sobre nosotros</Link></li>
+          <li className="nav-item"><Link className="nav-link text-white" to="/servicios">Servicios</Link></li>
+          <li className="nav-item"><Link className="nav-link text-white" to="/contacto">Contactanos</Link></li>
         </ul>
       </div>
     </nav>
