69bb031fa50b441909b471bd0a482e37f68283cb
AsleepSiren4516 <bobby4096@outlook.com>
2026-07-23 22:40:23 -0600

    Cambios Anahi

--- src/paginas/Donar.jsx
+++ src/paginas/Donar.jsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 import Donacion from "../componentes/Donacion";
-import filosofo from "../assets/ghandi.jpeg";
 import { FaHeart, FaLock, FaBoxOpen, FaHeartbeat, FaTshirt, FaPuzzlePiece, FaShoppingBasket, FaArrowCircleRight, FaBrain, FaWheelchair, FaPaintBrush } from "react-icons/fa";
 import { BsPersonWheelchair } from "react-icons/bs";
 import logoCorazon from "../assets/corazon_logo.svg";
@@ -9,30 +8,6 @@ export default function Donar(){
     return (
         <>
         <div className="donar">
-          <div className="donar-contenedor container">
-            <div className="row align-items-center g-4">
-
-              <div className="col-lg-6 order-2 position-relative">
-                <br />
-                <h2 className="donar-title">
-                  "La verdadera humanidad se muestra en cómo tratamos a los más vulnerables."
-                </h2>
-                <br />
-                <h5 className="donar-subtitle text-end">
-                  ~ Mahatma Gandhi
-                </h5>
-              </div>
-
-              <div className="col-lg-6 order-1 text-center">
-                <img
-                  src={filosofo}
-                  alt="Sobre nosotros"
-                  className="donar-image img-fluid"
-                />
-              </div>
-
-            </div>
-          </div>
         </div>
    
         <div className="container py-3">
